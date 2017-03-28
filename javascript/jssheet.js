297d91ca1480d232cc9f6a8b0adc748580f4d8c9
PJ Valentini <pj.valentini@gmail.com>
2017-03-28 18:24:31 -0400

    Add files via upload

--- javascript/jssheet.js
+++ javascript/jssheet.js
@@ -11,8 +11,8 @@ var meats = [burgers, hotDogs]
 //Use console.log to print to the console.
 
   console.log(meats)
-  console.log(meats[0][0]);
-  console.log(meats[1][0]);
+  console.log(meats[0][0]); //Indexes the proper string.
+  console.log(meats[1][0]); //Indexes the proper string.
 
 // 2. Declare a function that takes a name as an argument, and returns a new string that compliments that person.
 
@@ -28,13 +28,13 @@ console.log(person(" Joe "));
 
 // 3. Print the classic "99 bottles of beer on the wall" song to the console, in its entirety, using a for loop. Then make a copy of that, and rewrite it to use a while loop instead.
 
-// FOR LOOP of the song 99 bottles of beer...
+// (FOR) LOOP of the song 99 bottles of beer...
 
 for (var b = 99; b > 0; b--) {
   console.log(b + "bottles of beer on the wall");
 }
 
- //WHILE LOOP
+ //(WHILE) LOOP
 
 var b = 99;
   while (b >= 1){
@@ -45,9 +45,8 @@ var b = 99;
 
 // 4. Declare a function that takes in an array as an argument, and console logs out all of its elements backwards.
 
-// // //Declaring the array.
+// Declaring the array.
 var myArray = ["Juventus", "Lazio", "Roma", "AC Milan"];
-//Using the Reverse Command to reverse the order of the array.
 function arrayCounter(array) {
   for (var i = array.length - 1; i >= 0; --i) {
     console.log(array[i])
@@ -55,5 +54,5 @@ function arrayCounter(array) {
 }
 arrayCounter(myArray);
 
-//Using printConsole Function to print the reversed array in the console.
+//Using console.log to print the reversed array in the console.
  console.log(myArray);
